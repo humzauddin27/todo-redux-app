b1dfe0641f9dff5e373da46aace77fa6a38cb529
humzauddin27 <humzauddin7@gmail.com>
2017-09-11 17:50:05 -0400

    currently working on being able to create tasks into the mongo db and testing the api locally. for tomorrow/home, I want to have this finished.

--- python /models/dao.py
+++ python /models/dao.py
@@ -1,51 +1,28 @@
 from pymongo import MongoClient
 
-TODOS = [ 
-	{
-	'id': 1,
-	'todo': 'Achieve world peace',
-	'completed': false
-	},
-	{
-	'id': 2,
-	'todo': 'Cure cancer',
-	'completed': false
-	},
-	{
-	'id': 3,
-	'todo': 'Become a billionaire',
-	'completed': false
-	},
-	{
-	'id': 4,
-	'todo': 'Become a Pokemon master',
-	'completed': true
-	},
-	{
-	'id': 5,
-	'todo': 'Get a girlfriend',
-	'completed': false
-	},
-]
-
-client = MongoClient()
+client = MongoClient('mongodb://localhost:27017')
 db = client.test_database
 
 TODOS = db.todos
 
 class TaskDao:
 
-	def get_connection(self):
-
 	def get_task_by_id(self, task_id):
+		return TODOS.find({"id":task_id})
 
 	def get_task_by_params(self, kwargs):
+		#return TODOS.find({kwargs})
 
-	def create_task(self, task):
+	def create_task(self, task_id, task):
+		return TODOS.insert({
+			"id": task_id,
+			"task": task,
+			"completed": false
+			})
 
-	def update_user(self, task_id, task):
+	#def update_task(self, task_id, task):
 
-	def delete_user(self, task_id):
+	#def delete_task(self, task_id):
 
 
 
--- python /models/model.py
+++ python /models/model.py
@@ -3,6 +3,11 @@ from dao.py import TaskDao
 class Model:
 
 	def find_by_id(task_id):
+		dao = TaskDao()
+		task = dao.get_task_by_id(task_id)
+		model = Model(task) if model else None
+
+		return model
 
 	def find_by_params(kwargs):
 

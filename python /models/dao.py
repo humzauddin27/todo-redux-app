c3a7bc35b91df901b56296c8690dbf9d657a34f2
humzauddin27 <humzauddin7@gmail.com>
2017-09-11 17:11:57 -0400

    trying to implement the dao and model

--- python /models/dao.py
+++ python /models/dao.py
@@ -4,27 +4,27 @@ TODOS = [
 	{
 	'id': 1,
 	'todo': 'Achieve world peace',
-	'completed': False
+	'completed': false
 	},
 	{
 	'id': 2,
 	'todo': 'Cure cancer',
-	'completed': False
+	'completed': false
 	},
 	{
 	'id': 3,
 	'todo': 'Become a billionaire',
-	'completed': False
+	'completed': false
 	},
 	{
 	'id': 4,
 	'todo': 'Become a Pokemon master',
-	'completed': True
+	'completed': true
 	},
 	{
 	'id': 5,
 	'todo': 'Get a girlfriend',
-	'completed': False
+	'completed': false
 	},
 ]
 
@@ -32,6 +32,21 @@ client = MongoClient()
 db = client.test_database
 
 TODOS = db.todos
+
+class TaskDao:
+
+	def get_connection(self):
+
+	def get_task_by_id(self, task_id):
+
+	def get_task_by_params(self, kwargs):
+
+	def create_task(self, task):
+
+	def update_user(self, task_id, task):
+
+	def delete_user(self, task_id):
+
 
 
 
--- python /models/model.py
+++ python /models/model.py
@@ -1,3 +1,11 @@
-
+from dao.py import TaskDao
 
 class Model:
+
+	def find_by_id(task_id):
+
+	def find_by_params(kwargs):
+
+	def create_task(task):
+
+	def delete_task(task_id)

04875aabbebb09263a8dd4249aacf5c07752290d
humzauddin27 <humzauddin7@gmail.com>
2017-09-11 16:24:37 -0400

    more changes

--- python /app.py
+++ python /app.py
@@ -4,34 +4,6 @@ from flask_restful import reqparse, abort, Api, Resource
 app = Flask(__name__)
 api = Api(app)
 
-TODOS = [ 
-	{
-	'id': 1,
-	'todo': 'Achieve world peace',
-	'completed': False
-	},
-	{
-	'id': 2,
-	'todo': 'Cure cancer',
-	'completed': False
-	},
-	{
-	'id': 3,
-	'todo': 'Become a billionaire',
-	'completed': False
-	},
-	{
-	'id': 4,
-	'todo': 'Become a Pokemon master',
-	'completed': True
-	},
-	{
-	'id': 5,
-	'todo': 'Get a girlfriend',
-	'completed': False
-	},
-]
-
 parser = reqparse.RequestParser()
 parser.add_argument('todo')
 
--- python /models/dao.py
+++ python /models/dao.py
@@ -1,1 +1,38 @@
-aa
+from pymongo import MongoClient
+
+TODOS = [ 
+	{
+	'id': 1,
+	'todo': 'Achieve world peace',
+	'completed': False
+	},
+	{
+	'id': 2,
+	'todo': 'Cure cancer',
+	'completed': False
+	},
+	{
+	'id': 3,
+	'todo': 'Become a billionaire',
+	'completed': False
+	},
+	{
+	'id': 4,
+	'todo': 'Become a Pokemon master',
+	'completed': True
+	},
+	{
+	'id': 5,
+	'todo': 'Get a girlfriend',
+	'completed': False
+	},
+]
+
+client = MongoClient()
+db = client.test_database
+
+TODOS = db.todos
+
+
+
+
